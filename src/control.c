d0285f56992f8eda50949b18115ece8bebc19527
praveenkumarg204 <81771822+praveenkumarg204@users.noreply.github.com>
2021-04-29 21:27:26 +0530

    Update control.c

--- src/control.c
+++ src/control.c
@@ -24,7 +24,7 @@ int control(void){
 
 
     while(1){
-            if( !(PIND&(1<<PD0) && !(PIND&(1<<PD1))  ){
+            if( !(PIND&(1<<PD0)) && !(PIND&(1<<PD1))  ){
               PORTB |=(1<<PB1);
             }
             else{
