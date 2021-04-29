ed9ef29dedc2914d20b828bb1454574672e33e85
praveenkumarg204 <praveenkumarg204@gmail.com>
2021-04-29 15:46:43 +0530

    update

--- project_main.c
+++ project_main.c
@@ -1,36 +1,13 @@
-/**
- * @file project_main.c
- * @author Praveen Kumar G
- * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
- * @version 0.1
- * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-#include "project_config.h"
 #include "user_utils.h"
-#include "blinky.h"
-/**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
+#include "ADC.h"
 int main(void){
-	
-	//port configure
-    DDRB |=(1<<PB1);
-    DDRD &=~(1<<PD0);
-    DDRD &=~(1<<PD1);
-
-
+    
+    /* Activity 2*/
+    initADC();
+    uint16_t temperature;
     while(1){
-            if( !PIND&(1<<PD0) && !(PIND&(1<<PD1))  ){
-              PORTB |=(1<<PB1);
-            }
-            else{
-                PORTB &=~(1<<PB1);
-            }
-
+        temperature =readADC(0);
+        _delay_ms(200);
     }
     return 0;
 }
