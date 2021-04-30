79351e7ceb1a365d446811e285217a459c758610
praveenkumarg204 <praveenkumarg204@gmail.com>
2021-04-30 21:18:26 +0530

    update

--- src/usart.c
+++ src/usart.c
@@ -6,7 +6,7 @@ void initUSART(uint16_t ubrreg_value)
 {
     UBRR0L = ubrreg_value;
     UBRR0H = (ubrreg_value>>8)&(0x00ff);
-    UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ01);
+    UCSR0C = (1<<UMSEL00)|(1<<UCSZ01);
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 
 }
