b2c18e226ca7ed120340403349ed5ee8cdcbe709
praveenkumarg204 <praveenkumarg204@gmail.com>
2021-04-30 15:46:25 +0530

    update

--- project_main.c
+++ project_main.c
@@ -9,15 +9,14 @@ int main(void){
    
    /* Activity 1*/
      control();
-    /* Activity 2
+    /* Activity 2*/
     initADC();
     uint16_t temperature;
     while(1){
         temperature =readADC(0);
         _delay_ms(200);
     }
-  */
-    /*Activity 3
-   Timer_PWM();*/
+    /*Activity 3*/
+   Timer_PWM();
    return 0;
 }
