f726d7e3539aa140be05a67da01fd3c5f504ce8b
praveenkumarg204 <praveenkumarg204@gmail.com>
2021-04-30 16:21:21 +0530

    update

--- project_main.c
+++ project_main.c
@@ -5,18 +5,32 @@
 unsigned volatile overflow;
 
 int main(void){
-
-   
-   /* Activity 1*/
      control();
-    /* Activity 2*/
     initADC();
     uint16_t temperature;
+    Timer_PWM();
     while(1){
+        if( !(PIND&(1<<PD0)) && !(PIND&(1<<PD1))  ){
+              PORTB |=(1<<PB0);
+            }
+            else{
+                PORTB &=~(1<<PB0);
+            }
+
         temperature =readADC(0);
         _delay_ms(200);
+         OCR1A=10;
+        _delay_ms(200);
+        OCR1A=50;
+        _delay_ms(200);
+        OCR1A=100;
+        _delay_ms(200);
+        OCR1A=150;
+        _delay_ms(200);
+        OCR1A=200;
+        _delay_ms(200);
+        OCR1A=250;
+        _delay_ms(200);
     }
-    /*Activity 3*/
-   Timer_PWM();
    return 0;
 }
--- src/control.c
+++ src/control.c
@@ -21,16 +21,5 @@ int control(){
     DDRB |=(1<<PB0);
     DDRD &=~(1<<PD0);
     DDRD &=~(1<<PD1);
-
-
-    while(1){
-            if( !(PIND&(1<<PD0)) && !(PIND&(1<<PD1))  ){
-              PORTB |=(1<<PB0);
-            }
-            else{
-                PORTB &=~(1<<PB0);
-            }
-
-    }
     return 0;
 }
